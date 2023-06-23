224bb4f1e4a19d785cdd1b3460c0058f5b77bc27
Daniel <daespuor1991@gmail.com>
2023-06-22 21:35:12 -0500

    feat(#): Add component tests

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,5 +1,5 @@
-import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+// @vitest-environment happy-dom
+import { screen, render } from 'test/utilities';
 import Counter from '.';
 
 test('it should render the component', () => {
@@ -9,20 +9,28 @@ test('it should render the component', () => {
 });
 
 test('it should increment when the "Increment" button is pressed', async () => {
-  const user = userEvent.setup();
-  render(<Counter />);
-
+  const { user } = render(<Counter />);
   const currentCount = screen.getByTestId('current-count');
   const incrementButton = screen.getByRole('button', { name: 'Increment' });
-
   await user.click(incrementButton);
 
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  render(<Counter initialCount={5} />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('5');
+});
 
 test.todo(
   'it should reset the count when the "Reset" button is pressed',
-  async () => {},
+  async () => {
+    const { user } = render(<Counter initialCount={5} />);
+    const currentCount = screen.getByTestId('current-count');
+    const resetButton = screen.getByRole('button', { name: 'Reset' });
+    await user.click(resetButton);
+
+    expect(currentCount).toHaveTextContent('0');
+  },
 );
--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -10,19 +10,36 @@ it('has the correct title', async () => {
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  render(<PackingList />);
+  screen.getByLabelText(/Add New Item/i);
+});
 
-it.todo(
-  'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
-);
+it('has a "Add New Item" button that is disabled when the input is empty', () => {
+  render(<PackingList />);
+  const button = screen.getByRole('button', { name: /Add New Item/i });
+  const input = screen.getByLabelText('New Item Name');
+  expect(input).toHaveValue('');
+  expect(button).toBeDisabled();
+});
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('enables the "Add New Item" button when there is text in the input field', async () => {
+  const { user } = render(<PackingList />);
+  const button = screen.getByRole('button', { name: /Add New Item/i });
+  const input = screen.getByLabelText('New Item Name');
+  expect(input).toHaveValue('');
+  expect(button).toBeDisabled();
+  await user.type(input, 'Socks');
+  expect(input).toHaveValue('Socks');
+  expect(button).toBeEnabled();
+});
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const button = screen.getByRole('button', { name: /Add New Item/i });
+  const input = screen.getByLabelText('New Item Name');
+  await user.type(input, 'Socks');
+  await user.click(button);
+
+  expect(screen.getByLabelText('Socks')).not.toBeChecked();
+});
